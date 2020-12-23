2545ecf924de42f8484ca05415a13317f3b5365e
Phoebe Khokgawe <khokgawep@gmail.com>
2020-12-23 00:25:50 +0000

    Implement the query() to find free time for the meeting

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,93 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+    private ArrayList<TimeRange> freeTimeRange = new ArrayList<>();
+    ArrayList<Event> allEvents = new ArrayList<>();
+    private int index = 0;
+    private int validStartTime = TimeRange.START_OF_DAY;
+    private int freeTimeDuration = 0;
+
+    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+
+        //don't consider the event that the attendees are not joining
+        for(Event event : events){
+            if(attendeesIsInThisMeeting(event,request)){
+                this.allEvents.add(event);
+            }
+        }
+
+        if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) return new ArrayList<TimeRange>();
+        else if (request.getAttendees().isEmpty() || allEvents.isEmpty()) return Arrays.asList(TimeRange.WHOLE_DAY);
+        else {
+            //checking through all the events and find the free time during the day
+            for (Event event : allEvents) {
+                if(!attendeesIsInThisMeeting(event,request)){continue;}
+                validStartTime = checkReturnValidStartTime(event);
+                System.out.println("Valid start time before: " + validStartTime);
+                if (validStartTime > event.getWhen().start()) {
+                    checkLastEvent(allEvents.size());
+                    continue;
+                }
+                freeTimeRange.add(TimeRange.fromStartEnd(validStartTime, event.getWhen().start(), false));
+                validStartTime = event.getWhen().end();
+                System.out.println("Valid start time after: " + validStartTime);
+                checkLastEvent(allEvents.size());
+                System.out.println(freeTimeRange.toString());
+                index++;
+            }
+
+            //check that the free time that's given is enough for the person to have a meeting
+            for (TimeRange range : freeTimeRange) {
+                freeTimeDuration += range.duration();
+                if (freeTimeDuration >= request.getDuration()) return freeTimeRange;
+            }
+            //not enough free time
+            freeTimeRange.clear();
+
+        }
+        return freeTimeRange;
+    }
+
+    /**
+     * The valid start time is where there's no conflict within the given event.
+     * @param event -- current event
+     * @return if there's no conflict -- original validStartTime
+     *         if there's conflict -- ending time of the given event
+     */
+    private int checkReturnValidStartTime(Event event) {
+        return event.getWhen().contains(validStartTime) ? event.getWhen().end() : validStartTime;
+    }
+
+    /**
+     * The last event need to be added specifically if the END_OF_DAY time.
+     * @param size -- total valid events
+     */
+    private void checkLastEvent(int size) {
+        if (index == size - 1) { //last event
+            freeTimeRange.add(TimeRange.fromStartEnd(validStartTime, TimeRange.END_OF_DAY, true));
+        }
+    }
+
+    /**
+     * Check if the attendees in the current event is in the meeting.
+     * @param event -- current event
+     * @param request -- current meeting request
+     * @return true -- attendee is in the meeting
+     *         false -- attendee is not in the meeting
+     */
+    private boolean attendeesIsInThisMeeting(Event event, MeetingRequest request) {
+        for (String attendee : event.getAttendees()) {
+            if (!request.getAttendees().contains(attendee)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
+
